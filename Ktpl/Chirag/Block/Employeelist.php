d0e37f7885f40e8f88792f535c17f17eb0add47e
chirag.bhavsar <chirag.bhavsar@krishtechnolabs.com>
2015-10-08 17:29:38 +0530

    first commit

---
+++ Ktpl/Chirag/Block/Employeelist.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Ktpl\Chirag\Block;
+
+use Magento\Framework\View\Element\Template;
+
+/**
+ * Main contact form block
+ */
+class Employeelist extends Template
+{
+    /**
+     * @param Template\Context $context
+     * @param array $data
+     */
+    protected $_employeeFactory;
+    public function __construct(
+        \Magento\Framework\View\Element\Template\Context $context,
+        \Ktpl\Chirag\Model\EmployeeFactory $employeeFactory,
+        array $data = []
+     ) 
+    {
+        $this->_employeeFactory = $employeeFactory;
+        parent::__construct($context, $data);
+        //get collection of data 
+        $collection = $this->_employeeFactory->create()->getCollection();
+        $this->setCollection($collection);
+        $this->pageConfig->getTitle()->set(__('Employee List'));
+    }
+
+    protected function _prepareLayout()
+    {
+        parent::_prepareLayout();
+        if ($this->getCollection()) {
+            // create pager block for collection 
+            $pager = $this->getLayout()->createBlock(
+                'Magento\Theme\Block\Html\Pager',
+                'ktpl.chirag.record.pager'
+            )->setCollection(
+                $this->getCollection() // assign collection to pager
+            );
+            $this->setChild('pager', $pager);// set pager block in layout
+        }
+        return $this;
+    }
+  
+    /**
+     * @return string
+     */
+    // method for get pager html
+    public function getPagerHtml()
+    {
+        return $this->getChildHtml('pager');
+    } 
+    /*public function __construct(Template\Context $context, array $data = [])
+    {
+        parent::__construct($context, $data);
+        $this->_isScopePrivate = true;
+    }*/
+}
